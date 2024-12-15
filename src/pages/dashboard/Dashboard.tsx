35ddaed67f1c6670d0302ff0e620656245d50c00
anderson exner <anderson.exner@digitaldesk.com.br>
2024-12-15 18:35:42 -0300

    adjust import

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { Box } from "@mui/material";
 import { ArrowUpwardRounded as ArrowUpwardRoundedIcon, AccountBalanceRounded as AccountBalanceRoundedIcon, ArrowDownwardRounded as ArrowDownwardRoundedIcon } from '@mui/icons-material';
 
-
+import { InfoPaper } from "../../shared/components/infoPaper/InfoPaper";
 import { PieGraph } from "../../shared/components/Graphs/PieGraph";
-import { InfoPaper } from "../../shared/components/InfoPaper/InfoPaper";
 
 const receitas = [
     { label: 'Salário', value: 1500 },
